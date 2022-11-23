03a8f2967285bc1b85c8d31b1bdf1896d1b91331
Peter Toth <tothp0093@gmail.com>
2022-11-23 14:05:52 +0100

    NodeJs microservice base

--- src/Services/BookingService.Nodejs/services/booking.service.ts
+++ src/Services/BookingService.Nodejs/services/booking.service.ts
@@ -1,22 +1,68 @@
 import { Repository } from "typeorm";
-import { BookingRepository } from "../data/booking.repository";
 import { Booking } from "../models/booking";
+import { ParkingSlot } from "../models/parking-slot";
+import { AppDataSource } from "../data/data-source";
 
-class BookingService {
-    constructor(private _bookingRepository: Repository<Booking>) { }
+export class BookingService {
+    private _maximumAllowedBookingPeriod: number = 12 * 60 * 60 * 1000;
+
+    private _bookingRepository: Repository<Booking> = AppDataSource
+        .getRepository(Booking);
+    private _parkingSlotRepository: Repository<ParkingSlot> = AppDataSource.getRepository(ParkingSlot);
+
+    constructor() {
+
+    }
 
     async getUserBookings(userId: number): Promise<Booking[]> {
-        let bookings = this._bookingRepository.find();
+        let bookings = this._bookingRepository.findBy({
+            userId: userId
+        });
+
         return bookings;
     }
 
-    bookParkingSlot(userId: number, startDate: Date, endDate: Date): void {
+    async bookParkingSlot(userId: number, startDate: Date, endDate: Date): Promise<void> {
 
-        //TODO: get free parking spaces
+        if (startDate.getTime() + 60 * 60 * 1000 > endDate.getTime()
+            || endDate.getTime() - startDate.getTime() > this._maximumAllowedBookingPeriod){
+                return Promise.reject("Invalid start or end date of requested booking");
+            }
+
+        let parkingSlots = await this._parkingSlotRepository.find();
+        let bookingsForGivenTimePeriod = await AppDataSource
+            .getRepository(Booking)
+            .createQueryBuilder("booking")
+            .where("booking.startDate > :startDate AND booking.endDate > :endDate", { startDate: startDate, endDate: endDate})
+            .orWhere("booking.startDate > :startDate AND booking.endDate < :endDate", { startDate: startDate, endDate: endDate})
+            .orWhere("booking.startDate < :startDate AND booking.endDate > :endDate", { startDate: startDate, endDate: endDate})
+            .orWhere("booking.startDate < :startDate AND booking.endDate < :endDate", { startDate: startDate, endDate: endDate})
+            .getMany();
+
+        let currentlyBookedParkingSlots = bookingsForGivenTimePeriod.map(booking =>
+            booking.parkingSlotId).filter(this.uniqueFilter);
+        let freeSlots = parkingSlots.filter(parkingSlot =>
+            !currentlyBookedParkingSlots.includes(parkingSlot.id))
+
+        if (!freeSlots.length)
+        {
+            return Promise.reject("No free parking spots found");
+        }
 
         let booking = new Booking();
+        booking.parkingSlotId = freeSlots[0].id;
+        booking.userId = userId;
+        booking.startDate = startDate;
+        booking.endDate = endDate;
+
+        await this._bookingRepository.save(booking);
      }
 
-    async cancelBooking(userId: number, bookingId: number): Promise<void> {
+    async cancelBooking(bookingId: number): Promise<void> {
+        await this._bookingRepository.delete({ id: bookingId });
+    }
+
+    private uniqueFilter(value: number, index: number, self: number[]): boolean {
+        return self.indexOf(value) === index;
     }
  }
