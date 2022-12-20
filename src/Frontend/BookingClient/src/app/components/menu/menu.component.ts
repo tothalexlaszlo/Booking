a912d6dfe33819b48f875251239a6b57e817ce0e
Sára Krisztián <krisztian14@citromail.hu>
2022-12-20 15:33:36 +0100

    menu template initialized with comp settings

--- src/Frontend/BookingClient/src/app/components/menu/menu.component.ts
+++ src/Frontend/BookingClient/src/app/components/menu/menu.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'oidc-client-ts';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  @Input() public currentUser: User | null = null;
+
+  constructor(private readonly _authService: AuthService) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  login(): void {
+
+  }
+  logout(): void {}
 
 }
