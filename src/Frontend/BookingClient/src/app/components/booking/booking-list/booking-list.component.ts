bdadbbc9c03eed35bba6c07cbdade932d9e96934
Peter Toth <tothp0093@gmail.com>
2023-01-02 08:44:58 +0100

    Booking deletion basics

--- src/Frontend/BookingClient/src/app/components/booking/booking-list/booking-list.component.ts
+++ src/Frontend/BookingClient/src/app/components/booking/booking-list/booking-list.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { BehaviorSubject, combineLatest, map, Subject, Subscription } from 'rxjs';
 import { BookingService } from 'src/app/services/booking/booking.service';
 
 @Component({
   templateUrl: './booking-list.component.html',
   styleUrls: ['./booking-list.component.scss']
 })
-export class BookingListComponent {
-  public bookings$ = this._bookingService.bookingsByUser$;
+export class BookingListComponent implements OnDestroy {
+  private cancellationSubject = new BehaviorSubject<boolean>(true);
+  private _subscription: Subscription = Subscription.EMPTY;
+
+  public bookings$ = combineLatest([
+    this._bookingService.bookingsByUser$,
+    this.cancellationSubject
+  ]).pipe(map((value) => value[0]));
 
   constructor(private readonly _bookingService: BookingService) {
     console.log('BookingList');
   }
 
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
+
   public cancelBooking(bookingId: number) {
-    this._bookingService.cancelBooking(bookingId);
+    this._subscription.unsubscribe();
+    this._bookingService.cancelBooking(bookingId)
+      .subscribe({
+        next: () => this.cancellationSubject.next(true),
+        error: console.error
+      });
   }
 }
